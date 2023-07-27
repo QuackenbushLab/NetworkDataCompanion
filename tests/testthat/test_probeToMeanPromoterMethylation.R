a18e4a7ece8d2acb31ef39750ca176ecf78225f4
katehoffshutta <43797774+katehoffshutta@users.noreply.github.com>
2023-07-27 16:32:30 -0400

    updated probe mean methylation mapping docs and unit test

--- tests/testthat/test_probeToMeanPromoterMethylation.R
+++ tests/testthat/test_probeToMeanPromoterMethylation.R
@@ -44,7 +44,7 @@ test_that("all probes that map to a gene are extracted from the map",{
                                "CpG_beg"=3245,
                                "CpG_end"=3246,
                                "probe_strand"="+",
-                               "probeID"="cg00000005",
+                               "probeID"="cg00000004",
                                "genesUniq"="SEVERUS",
                                "geneNames"="SEVERUS",
                                "transcriptTypes"=NA,
@@ -66,8 +66,10 @@ test_that("all probes that map to a gene are extracted from the map",{
 
   write.table(manifest,file="testdata/manifest_test.tsv",sep="\t",row.names=F,quote=F)
   my_friend = NetSciDataCompanion::CreateNetSciDataCompanionObject()
-  my_map = my_friend$mapProbesToGenes(probelist = c("cg00000001","cg00000002"),
-                                           localManifestPath = "testdata/manifest_test.tsv")
+  my_map = my_friend$mapProbesToGenes(probelist = c("cg00000001","cg00000002","cg00000003","cg00000004"),
+                                      rangeUp = 200,
+                                      rangeDown = 200,
+                                      localManifestPath = "testdata/manifest_test.tsv")
   names(my_map)[2] = "geneNames" #TODO Add switch case for column names depending on manifest type
   my_friend$probeToMeanPromoterMethylation(methylation_betas = my_betas,
                                      genesOfInterest = c("HARRY","SEVERUS"),
@@ -75,5 +77,37 @@ test_that("all probes that map to a gene are extracted from the map",{
 })
 
 test_that("mean calculation is correct",{
+
+  # make toy beta values
+  my_betas = data.frame("probeID"=c("cg00000001",
+                                    "cg00000002",
+                                    "cg00000003",
+                                    "cg00000004"))
+
+  set.seed(42)
+  my_betas$minerva = runif(4)
+  my_betas$albus = runif(4)
+
+  my_friend = NetSciDataCompanion::CreateNetSciDataCompanionObject()
+  my_map = my_friend$mapProbesToGenes(probelist = c("cg00000001","cg00000002","cg00000003","cg00000004"),
+                                      rangeUp = 200,
+                                      rangeDown = 200,
+                                      localManifestPath = "testdata/manifest_test.tsv")
+  names(my_map)[2] = "geneNames"
+  mean_meth = my_friend$probeToMeanPromoterMethylation(methylation_betas = my_betas,
+                                           genesOfInterest = c("HARRY","SEVERUS"),
+                                           probe_gene_map = my_map)
+  minerva_mean_harry = mean(my_betas$minerva[1:3])
+  minerva_mean_severus = my_betas$minerva[4]
+  albus_mean_harry = mean(my_betas$albus[1:3])
+  albus_mean_severus = my_betas$albus[4]
+
+  manual_mean = matrix(c(minerva_mean_harry, minerva_mean_severus,
+                        albus_mean_harry, albus_mean_severus),
+                      ncol = 2, byrow=T)
+  row.names(manual_mean) = c("minerva","albus")
+  colnames(manual_mean)=c("HARRY","SEVERUS")
+
+  expect_equal(mean_meth, manual_mean)
 })
 
