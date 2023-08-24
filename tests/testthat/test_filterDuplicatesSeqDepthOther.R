fcb9d5502887581610a0f5e290e28b581ccc1a88
Soel Micheletti <soel.micheletti@hotmail.it>
2023-08-24 04:39:42 +0200

    Added tests for filterDuplicatesSeqDepthOther

---
+++ tests/testthat/test_filterDuplicatesSeqDepthOther.R
@@ -0,0 +1,17 @@
+context("[NetSciDataCompanion] Testing filterDuplicatesSeqDepthOther function ... ")
+
+test_that("Testing filterDuplicatesSeqDepthOther",{
+  
+  my_friend = NetSciDataCompanion::CreateNetSciDataCompanionObject()
+  barcode1 = "TCGA-A1-1234-01A"
+  barcode2 = "TCGA-A1-1234-11A"
+  barcode3 = "TCGA-A1-1234-21A"
+  barcode4 = "TCGA-A1-1234-01B"
+  barcode5 = "TCGA-A1-1234-21B"
+  barcode6 = "TCGA-A1-1234-21C"
+  barcodes = c(barcode1, barcode2, barcode3, barcode4, barcode5, barcode6)
+  
+  expr <- data.frame(rbind(c(1, 2, 3, 4, 5, 6), c(rep(1, 6))))
+  colnames(expr) <- barcodes
+  expect_equal(my_friend$filterDuplicatesSeqDepthOther(expr,barcodes), c(6))
+})
