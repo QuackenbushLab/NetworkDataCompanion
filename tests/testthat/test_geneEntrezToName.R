aed639a3ef6f587c017299abe99ef55cd9f633f8
cchen22 <chenchen9945@gmail.com>
2023-08-25 19:02:28 -0400

    Add tests for geneEntrezToName

---
+++ tests/testthat/test_geneEntrezToName.R
@@ -0,0 +1,15 @@
+context("[NetSciDataCompanion] Testing geneEntrezToName functions ... ")
+
+test_that("geneEntrezToName functions correctly converts Entrez to Name",{
+
+  my_friend = NetSciDataCompanion::CreateNetSciDataCompanionObject()
+
+  ## test genes:
+  ## MIF has two ids, TP53 has one id, and WNT3 has three ids.
+  gene_name = c("MIF","TP53","WNT3")
+  gene_entrez = c(4282,7157,7473)
+
+  out = my_friend$geneEntrezToName(gene_entrez)
+  expect_equal(out$gene_name,gene_name)
+
+})
